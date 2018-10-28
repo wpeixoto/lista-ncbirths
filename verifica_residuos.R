0e91aab57b6ce050882cc2518316662543fad90b
William Peixoto <william@sobek.nowhere>
2018-10-28 15:16:09 -0300

    Limite do que considerar "pequeno" agora é parâmetro

--- verifica_residuos.R
+++ verifica_residuos.R
@@ -1,15 +1,20 @@
-# Verificação de propriedades dos resíduos, ver pág. 110 e seguintes
+# Verificação de propriedades dos resíduos de regressão linear, 
+# ver pág. 110 e seguintes
 
-verifica_residuos = function(resids, obs, adjs) {
-  # P2                                                                 ====
+verifica_residuos = function(resids,  # vetor de resíduos
+                             obs,     # vetor de valores observados
+                             adjs,    # vetor de valores ajustados
+                             valor_pequeno=0.0000000001) 
+  {
+  # P2: soma dos resíduos é zero (se existir intercepto)               ====
   soma = sum(resids)
-  # P3                                                                 ====
+  # P3: a média dos resíduos é zero (se \beta_0 != 0)                   ====
   media = mean(resids)
-  # P4                                                                 ====
+  # P4: a soma dos observados é igual à dos ajustados                  ====
   diff_values = sum(obs) - sum(adjs)
-  # P5                                                                 ====
+  # P5: a média dos observados é igual à dos ajustados                 ====
   diff_medias = mean(obs) - mean(adjs)
-  # P7                                                                 ====
+  # P7: soma dos resíduos ponderada pelo valor ajustado é zero         ====
   soma_ponderada = sum(resids * adjs)
   
   result = list()
@@ -18,7 +23,7 @@ verifica_residuos = function(resids, obs, adjs) {
   result$diff.values.P4 = diff_values
   result$diff.medias.P5 = diff_medias
   result$soma.ponderada = soma_ponderada
-  results$is.small = (soma + media + diff_medias + diff_values + soma_ponderada < 0.0000000001)
+  results$is.small = (soma + media + diff_medias + diff_values + soma_ponderada < valor_pequeno)
   
   return(result)
   
