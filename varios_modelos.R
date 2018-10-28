bf1c7a3e88fc298291a1aa95a47c7b26365eb767
William Peixoto <william@sobek.nowhere>
2018-10-27 21:43:38 -0300

    Ensaio de unificação dos campos étnicos
    Não parece valer a pena. Guardar pra depois

--- varios_modelos.R
+++ varios_modelos.R
@@ -31,3 +31,15 @@ for (col in QUANTITATIVAS) {
 summary(lm(Weeks ~. -Low -Premie, data=ncb))
 summary(lm(Weeks ~ BirthWeightOz + Plural, data=ncb))
 
+# Unificar grupos étnicos em uma só coluna                              ====
+unique(paste(ncb$MomRace, ncb$RaceMom, ncb$HispMom))
+ncb <- cbind(ncb, Etnicidade = paste(ncb$MomRace, ncb$RaceMom, ncb$HispMom))
+ncb$MomRace = NULL
+ncb$RaceMom = NULL
+ncb$HispMom = NULL
+
+summary(lm(Weeks ~ ., data=ncb))
+summary(lm(Gained ~ ., data=ncb))
+summary(lm(BirthWeightOz ~ ., data=ncb))
+
+
